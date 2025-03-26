4eb0082d56e037101a72cc2729f52c4adf78a53b
felipesantos22 <felipevs2018@gmail.com>
2025-03-26 18:54:54 -0300

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Felipe Santos on 26/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
